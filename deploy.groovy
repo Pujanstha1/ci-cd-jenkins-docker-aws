d00b911b414d9b0aa20b6327430055a78fb530c1
Pujan Shrestha <pujanshrestha240@gmail.com>
2026-01-08 14:12:30 +0545

    Add: deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -7,7 +7,7 @@ pipeline {
         )
     }
     environment {
-        SSH_KEY = credentials('SSH_KEY')
+        //SSH_KEY = credentials('SSH_KEY')
         DOCKER_HUB_PASSWORD = credentials('DOCKER_HUB_TOKEN')
         SERVER_USER = 'ubuntu'
         DOCKER_HUB_USER = 'pujan240'
@@ -16,7 +16,7 @@ pipeline {
     }
 
     stages {
-        stage ('Build an Image') {
+        stage ('Build an Image and Push it to DockerHub') {
 
             steps {
 
@@ -32,6 +32,33 @@ pipeline {
                     }
             }
             
+        }
+
+        stage ('Deploy to EC2') {
+            withCredentials([
+                string(credentialsId: "SSH_KEY", variable:"SSH_KEY_FILE")
+            ]) {
+                sh '''
+                    set -e
+                    mkdir -p ~/.ssh
+                    chmod 700 ~/.ssh
+                    echo -e "Host *\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
+                    chmod 600 ~/.ssh/config
+
+                    echo "$SSH_KEY" | base64 -d >> mykey.pem
+                    chmod 400 mykey.pem 
+                    touch ~/.ssh/known_hosts 
+                    ssh-keygen -R "$SERVER_IP"
+
+                    scp -i mykey.pem ./docker-compose.yaml "$SERVER_NAME"@"SERVER_IP:~/"
+
+                    ssh -i mykey.pem "$SERVER_USER"@"$SERVER_IP" " 
+                    docker compose --env-file ./.env/dev_env pull 
+                    docker compose --env-file ./.env/dev_env down 
+                    docker compose --env-file ./.env/dev_env up -d 
+                    "
+                '''
+            }
         }
     }
 }
