58f195608c1846f6d263cf27ee068d68a8dcfc33
Pujan Shrestha <pujanshrestha240@gmail.com>
2026-01-08 14:28:55 +0545

    Fix: deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -35,30 +35,33 @@ pipeline {
         }
 
         stage ('Deploy to EC2') {
-            withCredentials([
-                string(credentialsId: "SSH_KEY", variable:"SSH_KEY_FILE")
-            ]) {
-                sh '''
-                    set -e
-                    mkdir -p ~/.ssh
-                    chmod 700 ~/.ssh
-                    echo -e "Host *\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
-                    chmod 600 ~/.ssh/config
 
-                    echo "$SSH_KEY" | base64 -d >> mykey.pem
-                    chmod 400 mykey.pem 
-                    touch ~/.ssh/known_hosts 
-                    ssh-keygen -R "$SERVER_IP"
+            steps {
+                withCredentials([
+                    string(credentialsId: "SSH_KEY", variable:"SSH_KEY_FILE")
+                ]) {
+                    sh '''
+                        set -e
+                        mkdir -p ~/.ssh
+                        chmod 700 ~/.ssh
+                        echo -e "Host *\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
+                        chmod 600 ~/.ssh/config
 
-                    scp -i mykey.pem ./docker-compose.yaml "$SERVER_NAME"@"SERVER_IP:~/"
+                        echo "$SSH_KEY" | base64 -d >> mykey.pem
+                        chmod 400 mykey.pem 
+                        touch ~/.ssh/known_hosts 
+                        ssh-keygen -R "$SERVER_IP"
 
-                    ssh -i mykey.pem "$SERVER_USER"@"$SERVER_IP" " 
-                    docker compose --env-file ./.env/dev_env pull 
-                    docker compose --env-file ./.env/dev_env down 
-                    docker compose --env-file ./.env/dev_env up -d 
-                    "
-                '''
-            }
+                        scp -i mykey.pem ./docker-compose.yaml "$SERVER_NAME"@"SERVER_IP:~/"
+
+                        ssh -i mykey.pem "$SERVER_USER"@"$SERVER_IP" " 
+                        docker compose --env-file ./.env/dev_env pull 
+                        docker compose --env-file ./.env/dev_env down 
+                        docker compose --env-file ./.env/dev_env up -d 
+                        "
+                    '''
+                }
+            }    
         }
     }
 }
