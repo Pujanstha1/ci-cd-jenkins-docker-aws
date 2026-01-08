7895f09f092cb08ba4a8063665a539f7a24a7ec8
Pujan Shrestha <pujanshrestha240@gmail.com>
2026-01-08 15:29:50 +0545

    scp'

--- deploy.groovy
+++ deploy.groovy
@@ -48,9 +48,9 @@ pipeline {
                         touch ~/.ssh/known_hosts 
                         ssh-keygen -R "$SERVER_IP"
 
-                        // scp -i $SSH_KEY ./docker-compose.yaml $SERVER_USER@$SERVER_IP:~/
+                        scp -i "$SSH_KEY" ./docker-compose.yaml $SERVER_USER@$SERVER_IP:~/
 
-                        ssh -i $SSH_KEY $SERVER_USER@$SERVER_IP " 
+                        ssh -i "$SSH_KEY" $SERVER_USER@$SERVER_IP " 
                         docker compose --env-file ./.env/dev_env pull 
                         docker compose --env-file ./.env/dev_env down 
                         docker compose --env-file ./.env/dev_env up -d 
