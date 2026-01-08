4f805eeaa9904534fa1d6c1f40ec382a6f23d6ba
Pujan Shrestha <pujanshrestha240@gmail.com>
2026-01-08 17:31:21 +0545

    fix: scp code

--- deploy.groovy
+++ deploy.groovy
@@ -14,23 +14,23 @@ pipeline {
     }
 
     stages {
-        // stage ('Build an Image and Push it to DockerHub') {
+        stage ('Build an Image and Push it to DockerHub') {
 
-        //     steps {
+            steps {
 
-        //         withCredentials([
-        //         string(credentialsId: "DOCKER_HUB_PASSWORD", variable: "DOCKER_HUB_PASSWORD")
-        //         ]) {
-        //             sh '''
-        //                 set -e
-        //                 echo "$DOCKER_HUB_PASSWORD" | docker login -u $DOCKER_HUB_USER --password-stdin
-        //                 docker build -t "$DOCKER_HUB_USER/$DOCKER_HUB_REPO:$TAG" .
-        //                 docker push "$DOCKER_HUB_USER/$DOCKER_HUB_REPO:$TAG"
-        //             '''
-        //             }
-        //     }
+                withCredentials([
+                string(credentialsId: "DOCKER_HUB_PASSWORD", variable: "DOCKER_HUB_PASSWORD")
+                ]) {
+                    sh '''
+                        set -e
+                        echo "$DOCKER_HUB_PASSWORD" | docker login -u $DOCKER_HUB_USER --password-stdin
+                        docker build -t "$DOCKER_HUB_USER/$DOCKER_HUB_REPO:$TAG" .
+                        docker push "$DOCKER_HUB_USER/$DOCKER_HUB_REPO:$TAG"
+                    '''
+                    }
+            }
             
-        // }
+        }
 
         stage ('Deploy to EC2') {
 
@@ -39,22 +39,26 @@ pipeline {
                     file(credentialsId: "SSH_KEY_FILE", variable:"SSH_KEY")
                 ]) {
                     sh '''
-                       
+                        set -e
                         mkdir -p ~/.ssh
                         chmod 700 ~/.ssh
+
+                        rm -f ~/.ssh/config
 
                         touch ~/.ssh/known_hosts 
                         ssh-keygen -R "$SERVER_IP"
 
-                        scp -i "$SSH_KEY" -o StrictHostKeyChecking=no \
-                         ./docker-compose.yaml $SERVER_USER@$SERVER_IP:~/ 
+                        scp -i "$SSH_KEY" \
+                            -o StrictHostKeyChecking=no \
+                            docker-compose.yaml \
+                            "$SERVER_USER@$SERVER_IP:~/"
 
                         ssh -i "$SSH_KEY" \
                         -o StrictHostKeyChecking=no \
                         $SERVER_USER@$SERVER_IP " 
-                        docker compose --env-file ./.env/dev_env pull 
-                        docker compose --env-file ./.env/dev_env down 
-                        docker compose --env-file ./.env/dev_env up -d 
+                            docker compose --env-file ./.env/dev_env pull 
+                            docker compose --env-file ./.env/dev_env down 
+                            docker compose --env-file ./.env/dev_env up -d 
                         "
                     '''
                 }
